fe6708cb3ebb5a14c9a56fa96752327092ab00a3
Gabriel Braga <kevencript@gmail.com>
2020-05-11 21:29:27 -0300

    [FEATURE] Authentication Route

--- lib/routes/Router.ts
+++ lib/routes/Router.ts
@@ -7,6 +7,7 @@
 
 import * as express from "express";
 import { Users } from "./users";
+import { Auth } from "./auth";
 
 export class Router {
   constructor() {
@@ -16,10 +17,12 @@ export class Router {
   // Instancing Modules
   public router: express.Router;
   public Users: Users = new Users();
+  public Auth: Auth = new Auth();
 
   // Defining Routes
   public getRouter() {
     this.router.use("/users", this.Users.getRouter());
+    this.router.use("/auth", this.Auth.getRouter());
 
     return this.router;
   }
