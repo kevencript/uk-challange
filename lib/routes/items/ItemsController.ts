325d98499c4cb235b49987ad6e49af4ac745bd9c
Gabriel Braga <kevencript@gmail.com>
2020-05-13 00:31:35 -0300

    [FEATURE] Add new Item

--- lib/routes/items/ItemsController.ts
+++ lib/routes/items/ItemsController.ts
@@ -23,10 +23,38 @@ export class ItemsController {
       const page_size = req.query.page_size ? req.query.page_size : 10;
 
       // Returning paginated data
-      var paginatedData = await itemsHelpers.paginateSelect(+page, +page_size);
-      paginatedData.page = +page;
+      var paginatedData = await itemsHelpers.paginateSelect(page, page_size);
 
       res.json(paginatedData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        errors: [{ msg: "Server Error", callback: err.message }],
+      });
+    }
+  }
+
+  //////////////////////////////////////
+  // @route    POST /items            //
+  // @desc     Add new Item           //
+  //////////////////////////////////////
+  public async addItem(req: Request, res: Response) {
+    try {
+      // Instancing new Helper
+      const itemsHelpers = new ItemsHelpers();
+
+      // Validating data and returning they padronized
+      await itemsHelpers.validatorAddItem(req.body);
+
+      // Inserting item
+      const createdItemId = await itemsHelpers.insertItem(req.body.name);
+
+      // Insering Multiple Groups
+      await itemsHelpers.insertGroupArray(req.body.groups, createdItemId);
+
+      res.json({
+        successMessage: "Item created!",
+      });
     } catch (err) {
       console.error(err);
       res.status(500).json({
